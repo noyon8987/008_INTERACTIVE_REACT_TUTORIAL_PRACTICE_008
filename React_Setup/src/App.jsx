4f1a01a7e53387e2ce707c98172d493e0fcfd84e
Shahadat Hosen Noyon <shahadathosen8987@gmail.com>
2025-05-27 21:27:03 +0600

    react tutorial 1 is added

--- React_Setup/src/App.jsx
+++ React_Setup/src/App.jsx
@@ -1,19 +1,9 @@
-import React from "react";
 import "./style.css";
-
-import Component from "./components/Component";
-import State from "./State/State";
-import Note_APP from "./Note Take App/Note_APP";
-import Student_App from "./Student Attendence App/Student_App";
-
 
 export default function App() {
   return (
     <div>
-      {/* <Component/> */}
-      {/* <State/> */}
-      {/* <Note_APP/> */}
-      <Student_App/>
+      <h1>Hello World.!</h1>
     </div>
   );
 }
