824f8a2995820d174a4a9977a19095c1d47fe32b
Shahadat Hosen Noyon <shahadathosen8987@gmail.com>
2025-05-29 15:21:44 +0600

    BioData Componant is Added

---
+++ React_Tutorial/src/components/BioData.jsx
@@ -0,0 +1,36 @@
+import "../style.css"
+
+export default function BioData(props) {
+  return (
+    <>
+      <div className="bioData">
+        <h2>Bio Data of {props.name}</h2>
+        <br />
+        <div className="personal_information">
+          <p>Email : {props.email}</p>
+          <p>Age: {props.age}</p>
+          <p>Phone: {props.phone}</p>
+          <p>Github: {props.github}</p>
+        </div>
+        <br />
+        <h2>My Skills</h2>
+        <div className="my-skills">
+          <ul>
+            {props?.skills.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+        <br />
+        <h2>My Interests</h2>
+        <div className="my-interests">
+          <ul>
+            {props?.interests?.map((interest) => (
+              <li key={interest}>{interest}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
