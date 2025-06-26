5b646b97b9510fe9d5de8d9c9bb8a28863448f9b
Shahadat Hosen Noyon <shahadathosen8987@gmail.com>
2025-06-26 22:48:54 +0600

    code update

--- React_Tutorial/src/App.jsx
+++ React_Tutorial/src/App.jsx
@@ -1,23 +1,23 @@
-import "./style.css"
+import "./style.css";
 
-import Props_2 from "./components/Props_2";
-import Conditional_Randering from "./components/Conditional_Randering";
-import ForEach from "./components/ForEach";
-import Map from "./components/Map";
-import Filter from "./components/Filter";
-import Count_State from "./state_components/Count_State";
-import Array_State from "./state_components/Array_State";
+// import Props_2 from "./components/Props_2";
+// import Conditional_Randering from "./components/Conditional_Randering";
+// import ForEach from "./components/ForEach";
+// import Map from "./components/Map";
+// import Filter from "./components/Filter";
+// import Count_State from "./state_components/Count_State";
+// import Array_State from "./state_components/Array_State";
 
 export default function App() {
   return (
     <>
-    {/* <Props_2/> */}
-    {/* <Conditional_Randering/> */}
-    {/* <ForEach/> */}
-    {/* <Map/> */}
-    {/* <Filter/> */}
-    {/* <Count_State/> */}
-    <Array_State/>
+      {/* <Props_2/> */}
+      {/* <Conditional_Randering/> */}
+      {/* <ForEach/> */}
+      {/* <Map/> */}
+      {/* <Filter/> */}
+      {/* <Count_State/> */}
+      {/* <Array_State/> */}
     </>
-  )
+  );
 }
